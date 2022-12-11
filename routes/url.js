28cae6cbf7a32a40c6956d2f8cabe675bac0a9f8
Mayank-Vishnoi <f20190098@pilani.bits-pilani.ac.in>
2022-12-11 16:24:35 +0530

    fixed a try catch and added route for allowing custom url

--- routes/url.js
+++ routes/url.js
@@ -29,15 +29,57 @@ router.post('/shorten', async (req, res) => {
                // Create url code
                const urlCode = encoder.base62_encode(cnt.counter);
                const shortUrl = baseUrl + '/' + urlCode;
+               try {
+                  await Url.create({
+                     full: fullUrl,
+                     short: shortUrl,
+                  });
+   
+                  res.redirect('/');
+               } catch (err) {
+                  res.status(500).json('Server error while adding a document to urls');
+               }
+            } catch (err) {
+               console.error(err);
+               res.status(500).json('Server error while accessing counter variable');
+            }
+         }
+      } catch (err) {
+         console.error(err);
+         res.status(500).json('Server error while accessing long url');
+      }
+   } else {
+      res.status(401).json('Invalid long url');
+   }
+});
+
+
+// handle custom urls.
+router.post('/custom', async (req, res) => {
+   const {fullUrl, shortId} = req.body;
+   const baseUrl = config.get('baseUrl');
+   const customUrl = baseUrl + '/' + shortId;
+
+   if (shortId.length <= 6) {
+      res.status(401).json('Please enter a custom Id with length greater than or equal to seven and try again!');
+   } else if (validUrl.isUri(fullUrl)) {
+      try {
+         let url = await Url.findOne({ full: fullUrl });
+         if (url) {
+            url.clicks = 0; // reset clicks
+            url.short = customUrl;
+            url.save();
+            res.redirect('/');
+         } else {
+            try {
                await Url.create({
                   full: fullUrl,
-                  short: shortUrl,
+                  short: customUrl,
                });
 
                res.redirect('/');
             } catch (err) {
-               console.error(err);
-               res.status(500).json('Server error while accessing counter variable');
+               res.status(500).json('Server error while adding a document to urls');
             }
          }
       } catch (err) {
