baebd67c0dbadee9829a1e966a120cc8cb4164f8
Mayank-Vishnoi <f20190098@pilani.bits-pilani.ac.in>
2022-12-13 16:18:01 +0530

    userModel naming

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,5 @@
 const LocalStrategy = require('passport-local').Strategy;
-const User = require('../models/User');
+const User = require('../models/UserModel');
 const bcrypt = require('bcrypt');
 
 module.exports = async function (passport) {
